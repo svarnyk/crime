903c04251344e4c86cc3c7dbddde2063aefffb67
svarnyk <svarnyk@gmail.com>
2022-09-29 17:09:11 +0300

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 const crimeBlock = document.querySelector('.crimeTopography');
+getCrime()
 
 function getCrime() {
-
+    const template = `
+      <div class="crimeTopography__wrapper">
+        <div class="crimeTopography__header">Crime Topography</div>
+        <div class="crimeTopography__content">
+            <div class="crimeTopography__statistic">
+                <div class="topic">
+                    <div class="topic__value">1234</div>
+                    <div class="topic__heading">Killed Militarists</div>
+                </div>
+                <div class="topic">
+                    <div class="topic__value">1234</div>
+                    <div class="topic__heading">Killed Militarists</div>
+                </div>
+                <div class="topic">
+                    <div class="topic__value">1234</div>
+                    <div class="topic__heading">Killed Militarists</div>
+                </div>
+                <div class="topic">
+                    <div class="topic__value">1234</div>
+                    <div class="topic__heading">Killed Militarists</div>
+                </div>
+            </div>
+            <div class="crimeTopography__map"></div>
+        </div>
+        <div class="crimeTopography__line"></div>
+    </div>
+`;
+    crimeBlock.innerHTML = template
 }
